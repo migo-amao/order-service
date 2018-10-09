b6c576001a1117cb174abfde9bf24481f9b95244
cuan <cuan_jun@hotmail.com>
2018-10-08 23:03:48 -0500

    Connection pool for feign client

--- src/main/java/wei/config/AppConfig.java
+++ src/main/java/wei/config/AppConfig.java
@@ -1,5 +1,7 @@
 package wei.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +10,14 @@ import java.util.Date;
 @Component
 @ConfigurationProperties(prefix = "app")
 public class AppConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
+
     private String id;
 
     public String getId() {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("In app config");
+        }
         return id;
     }
 
