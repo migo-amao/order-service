8c6e6a7f97b6dd715b13a00f099706d0814adddc
Wei Mao <wei@seapinetech.com>
2020-05-29 16:42:10 -0500

    Changed to webflux app.

--- src/main/java/wei/Application.java
+++ src/main/java/wei/Application.java
@@ -2,17 +2,10 @@ package wei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@EnableFeignClients
-@EnableGlobalMethodSecurity(
-		prePostEnabled = true,
-		securedEnabled = true,
-		jsr250Enabled = true)
+@EnableReactiveMethodSecurity
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/wei/web/resource/OrderResource.java
+++ src/main/java/wei/web/resource/OrderResource.java
@@ -1,41 +1,40 @@
 package wei.web.resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-import wei.config.AppConfig;
-/*import wei.web.client.AccountService;
-import wei.web.client.StockService;*/
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 import wei.web.domain.Account;
 import wei.web.domain.Order;
 
 @RestController
+@RequestMapping("/order-service")
 public class OrderResource {
 
-    /*@Autowired
-    private AccountService accountService;
-
-    @Autowired
-    private StockService stockService;
-*/
     @PreAuthorize("hasAuthority('SCOPE_ORDER_READ')")
     @GetMapping("/orders/{id}")
-    public Order getOrder(@PathVariable String id) {
+    public Mono<Order> getOrder(@PathVariable String id, @AuthenticationPrincipal Authentication authenticationPrincipal) {
+        // authentication will be null in reactive mode since the thread local that holds the security context doesn't apply
+        // in the reactive mode
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Order order = new Order();
         order.setId(id);
-        return order;
+        return Mono.just(order);
     }
 
     @PreAuthorize("hasAuthority('SCOPE_ORDER_WRITE')")
     @PostMapping("/orders")
     public void createOrder(@RequestBody Order order) {
         try {
-            Account account = new Account(); //accountService.findAccount("1");
-            int qty = 1; //stockService.findStock("2");
+            Account account = new Account();
+            int qty = 1;
             System.out.println("Account for order = " + account.getId() + ", qty = " + qty);
         } catch (Exception ex) {
             ex.printStackTrace();
